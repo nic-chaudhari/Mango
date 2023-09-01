ab9b47167695dbce2297e605c54c637c00c5ace4
Nirav_chaudhari <niravch138@gmail.com>
2023-09-01 14:58:05 +0530

    Add project files.

---
+++ Mango.Web/Models/CouponDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Mango.Web.Models
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+
+        public static implicit operator string(CouponDto v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿using static Mango.Web.Utility.SD;
+using System.Security.AccessControl;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public string Data { get; set; }
+        public string AccessToken { get; set; } 
+    }
+}
